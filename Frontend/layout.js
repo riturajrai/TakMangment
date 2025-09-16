807adb4c61225d122ffb793ab2569bf715b69826
Rituraj Rai <162144998+riturajrai@users.noreply.github.com>
2025-09-17 04:25:24 +0530

    Update layout.js

--- Frontend/layout.js
+++ Frontend/layout.js
@@ -12,11 +12,10 @@ const inter = Inter({ subsets: ["latin"], weight: ["400", "600", "700"] });
 export default function RootLayout({ children }) {
   const pathname = usePathname();
 
-  // Jin routes pe navbar/footer nahi chahiye
+
   const hideLayoutRoutes = ["/login", "/register", "/reset-password"];
   const shouldHideLayout = hideLayoutRoutes.includes(pathname);
 
-  // Jin routes pe sirf footer nahi chahiye
   const hideFooterRoutes = ["/profile/name" , '/profile/email' , '/profile/other', '/projects'];
   const shouldHideFooter = hideFooterRoutes.includes(pathname);
 
